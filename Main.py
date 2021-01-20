3148472281d7b2133b17586a3aed4a1544168741
Nikolas <38259407+Nikolai558@users.noreply.github.com>
2021-01-19 19:21:31 -0700

    Fix bug #1
    Fix bug #1

--- Main.py
+++ Main.py
@@ -64,6 +64,76 @@ def write_geoMap(diagram_name, diagram_dictionary, save_file_path="output.xml"):
                         "        </GeoMapObject>\n")
 
 
+def write_sct2(diagram_name, diagram_dictionary, save_file_path="output.sct2"):
+    with open(save_file_path, "w") as file:
+        file.writelines(f"- {diagram_name}{' '*(24-len(diagram_name))}N000.00.00.000 E000.00.00.000 N000.00.00.000 E000.00.00.000\n")
+
+        for diagram, coordinates in diagram_dictionary.items():
+            index_count = 0
+            # coordinate_pair[index_count + 1]
+
+            for coordinate_pair in coordinates:
+                try:
+
+                    start_lat = convert_decimal(float(coordinate_pair.split(",")[1]))
+                    start_lon = convert_decimal(float(coordinate_pair.split(",")[0]), False)
+
+                    end_lat = convert_decimal(float(coordinates[index_count + 1].split(",")[1]))
+                    end_lon = convert_decimal(float(coordinates[index_count + 1].split(",")[0]), False)
+
+                    file.writelines(f"{' '*26}{start_lat} {start_lon} {end_lat} {end_lon}\n")
+
+                except IndexError:
+                    pass
+
+                index_count += 1
+
+
+def convert_decimal(dd, lat=True):
+    negative = dd < 0
+    dd = abs(dd)
+    minutes, seconds = divmod(dd*3600, 60)
+    degrees, minutes = divmod(minutes, 60)
+    if negative:
+        if degrees > 0:
+            degrees = -degrees
+        elif minutes > 0:
+            minutes = -minutes
+        else:
+            seconds = -seconds
+
+    milliseconds = str(round(seconds, 3)).split('.')[1]
+    degrees = str(degrees).replace('.0', '')
+    minutes = str(minutes).replace('.0', '')
+    seconds = str(seconds).split('.')[0]
+
+    if len(degrees.replace('-', '')) < 3:
+        degrees = f"{0*(len(milliseconds)-3)}{degrees}"
+    if len(minutes) < 2:
+        minutes = f"{0*(len(minutes)-2)}{minutes}"
+    if len(seconds) < 2:
+        seconds = f"{0*(len(seconds)-2)}{seconds}"
+    if len(milliseconds) < 3:
+        milliseconds = f"{milliseconds}{0*(len(milliseconds)-3)}"
+
+    dms = f"{degrees}.{minutes}.{seconds}.{milliseconds}"
+
+    if lat:
+        # N or S
+        if negative:
+            dms = f"S{dms[1:]}"
+        else:
+            dms = f"N{dms}"
+    else:
+        # W or E
+        if negative:
+            dms = f"W{dms[1:]}"
+        else:
+            dms = f"E{dms}"
+
+    return dms
+
+
 if __name__ == '__main__':
 
     # exe_name.exe airport_name file_path
@@ -74,11 +144,12 @@ if __name__ == '__main__':
 
     # TODO Need to grab save directory path
 
-    if len(sys.argv) != 3:
+    if len(sys.argv) == 3:
         name = sys.argv[1]
         read_file_path = sys.argv[2]
 
-        write_geoMap(name, read_xml(read_file_path))
+        write_geoMap(name, read_xml(read_file_path), f"{name}.xml")
+        # write_sct2(name, read_xml(read_file_path), f"{name}.sct2")
     else:
         print("Your arguments are invalid. Try again.")
         print("Correct format is:\n"
